d08589efafa2c87e7f1a6a159fbb41091db934b7
Tommy Hojnicki <tjhojnicki@stu.naperville203.org>
2018-09-06 11:51:56 -0500

    adding junk

---
+++ again another test/Program1.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Program1 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Program1
+{
+    public static void main(String[] args)
+    {
+        System.out.println(" Hi");
+    }
+}
---
+++ functionTest.py
@@ -0,0 +1,12 @@
+#Tommy Hojnicki
+#September 4,2018
+#Using git for the first time
+
+def up(x):
+    print (x)
+    return x
+
+def down(x):
+    print(x-1)
+    return x-1
+    
