567cc9d6ed044522c5754011aedf58cf26243a1f
Tommy Hojnicki <tjhojnicki@stu.naperville203.org>
2018-09-07 12:18:15 -0500

    Beginning the parking program

---
+++ again another test/parkit.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+
+/**
+ * Write a description of class parkit here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class parkit
+{
+    public static void main(String[] args)
+    {
+        Stack <Integer> driveway = new Stack<>();
+        Stack <Integer> street= new Stack<>();
+        int cont=1;
+        
+    }
+}
