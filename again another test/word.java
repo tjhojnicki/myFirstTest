a1a5ebee172aafdb319f791573ca0c4cdce9e1b2
Tommy Hojnicki <tjhojnicki@stu.naperville203.org>
2018-09-12 12:57:51 -0500

    Finishing word

--- again another test/word.java
+++ again another test/word.java
@@ -12,15 +12,23 @@ public class word
 {
      public static void main(String [] args)
     {
-    
+        int count=0;
         TreeSet<String> words = new TreeSet<>();
         String filename="Text.txt";
         try(Scanner in = new Scanner(new File(filename)))
         {
             while(in.hasNext())
             {
-                
+             String word= in.next();
+             if(word.contains(".")||word.contains(","))
+                {
+                    word=word.substring(word.length()-1);
+                }
+             words.add(word.toLowerCase());
+             count++;
             }
+            System.out.println("There are "+ count+ " words in the document");
+            System.out.println("There are "+words.size()+ " unique words in the document");
         
         }
         catch(FileNotFoundException e)
